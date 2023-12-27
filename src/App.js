5f47e5526dd7e0c0257acb8a26209c6f525a1aeb
Brian Done <briguy2078@gmail.com>
2023-12-26 23:00:42 -0500

    added component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Data from './components/data/Data';
+import Cloud from './components/cloud/Cloud';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Navbar />
     <Hero />
     <Data />
+    <Cloud />
     
     </>
 
