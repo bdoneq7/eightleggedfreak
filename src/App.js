e67fa148f5ac4357a09fcc5174fc8aabc5a87b32
Brian Done <briguy2078@gmail.com>
2023-12-26 21:07:26 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import Hero from './components/hero/Hero';
+import Navbar from './components/navbar/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div class="container-one">
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            mollitia, consequuntur aliquid nobis vitae soluta maiores expedita ipsam
-            delectus molestiae!
-        </p>
-    </div>
-      
-    <div class="container-two">
-        <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-        architecto temporibus sed officiis vero, quisquam, corrupti quis veritatis
-        dolor amet nostrum quam! Voluptates nam architecto enim neque nemo
-        consectetur molestias unde fugit dolorum alias temporibus expedita
-        doloribus deserunt laborum asperiores illum saepe, voluptate rerum quia
-        sit facilis consequuntur perferendis aperiam. Nobis reiciendis debitis
-        consequuntur placeat maiores voluptas, quos esse eum.
-        </p>
-    </div>
+    
+    <>
+    <Navbar />
+    <Hero />
+    
+    </>
 
-    <div class="container-three">
-        <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus fugiat,
-        nemo rem facere cumque error. Aliquam consequatur nobis cupiditate atque!
-        Fugiat amet facere magni, nulla pariatur ut ullam, vel est eum voluptatum
-        dicta quis dignissimos labore repellendus. Maiores deserunt quas tempore
-        impedit, corporis quae amet blanditiis voluptatum laudantium magni ipsa!
-        </p>
-    </div> 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        
+      
+  
   );
 }
 
