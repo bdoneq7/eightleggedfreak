83d98bee5a66f43b4abd668ea2d9a809546e2c57
Brian Done <briguy2078@gmail.com>
2023-12-26 22:14:45 -0500

    added data component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
+import Data from './components/data/Data';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
     <Navbar />
     <Hero />
+    <Data />
     
     </>
 
---
+++ src/components/data/Data.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './DataStyles.css';
+
+const Data = () => {
+    return  (
+        <div className='data'>
+            <div className='container'>
+                <div className='content'>
+                    <h2><span>Data</span> Recovery</h2>
+                    <p>Text</p>
+                    <div><button>Sign Up</button></div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Data;
