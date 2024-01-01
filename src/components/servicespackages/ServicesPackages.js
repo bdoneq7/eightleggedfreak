fa9fd21ee4272c971c0abe0c47d23e7d935ccf18
Brian Done <briguy2078@gmail.com>
2023-12-31 19:01:10 -0500

    updated home styles

--- src/components/servicespackages/ServicesPackages.js
+++ src/components/servicespackages/ServicesPackages.js
@@ -11,7 +11,13 @@ const ServicesPackages = () => {
                 <div className='content'>
                     <h2><FaReact className='iconmain' /><span> Web Development</span> Packages</h2>
                     <p>
-                    We offer 4 packages that include Website Development, Logo Design, Website Hosting, and Website Maintenance.<br/>
+                    We offer 4 Web Development Packages, 4 Website Hosting Plans, and 4 Website Maintenance Plans:<br/>
+                    <br/>
+                    Our Basic Web Development Package starts at $1,399<br/>
+                    <br/>
+                    Our Basic Website Hosting Plan starts at $50/month<br/>
+                    <br/>
+                    Our Basic Website Maintenance Plan starts at $50/month<br/>
                     </p>
                     
                     
