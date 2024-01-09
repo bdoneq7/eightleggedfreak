03863116c3891ae00ac4c0e47994f7ae2ec76ec5
Brian Done <briguy2078@gmail.com>
2024-01-09 17:29:37 -0500

    services

--- src/components/servicespackages/ServicesPackages.js
+++ src/components/servicespackages/ServicesPackages.js
@@ -11,13 +11,15 @@ const ServicesPackages = () => {
                 <div className='content'>
                     <h2><FaReact className='iconmain' /><span> Web Development</span> Packages</h2>
                     <p>
-                    We offer 4 Web Development Packages, 4 Website Hosting Plans, and 4 Website Maintenance Plans:<br/>
+                    8LeggedFreak offers 4 Web Development Packages and 4 Website Hosting Plans / Maintenance Plans.<br/>
+                    Web Development Packages require paying a 50% Deposit of the total package price before work can begin
+                    on your website. After your website has been completed according to our agreed upon contract, then you
+                    will pay the remaining 50% of the web development package cost.
                     <br/>
-                    Our Basic Web Development Package starts at $1,399<br/>
                     <br/>
-                    Our Basic Website Hosting Plan starts at $50/month<br/>
+                    Web Development Packages begin at $1,399<br/>
                     <br/>
-                    Our Basic Website Maintenance Plan starts at $50/month<br/>
+                    Website Hosting and Post-Development Website Maintenance Plans begin at $99/month<br/>
                     </p>
                     
                     
