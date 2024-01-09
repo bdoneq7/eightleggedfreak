e65d64f787d40dcff9114f797a069b05be412244
Brian Done <briguy2078@gmail.com>
2024-01-09 14:05:59 -0500

    client image

--- src/components/clientportfolio/ClientPortfolio.js
+++ src/components/clientportfolio/ClientPortfolio.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ClientPortfolioStyles.css';
 import { CgWebsite } from "react-icons/cg";
+import NovaAshArtistry from "../../assets/novaashartistry.png";
 
 const ClientPortfolio = () => {
     return  (
@@ -8,7 +9,8 @@ const ClientPortfolio = () => {
           <div className='container'>
             <div className='content'>
                 <h2><CgWebsite className='iconmain' /><span> Website</span> Portfolio</h2>
-                <p>Nova Ash Artistry Card
+                <p>Nova Ash Artistry<br/>
+                <img src={NovaAshArtistry} style={{ width: "100%", height: "100%" }} alt="Nova Ash Artistry" title="Nova Ash Artistry" />
                 </p>
 
                 <p>Joey Joey Imagination Station Card
