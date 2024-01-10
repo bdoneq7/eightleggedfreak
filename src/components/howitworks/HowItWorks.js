2f25e605b6be07c084c28f8e801b7573454ceb78
Brian Done <briguy2078@gmail.com>
2024-01-09 20:17:30 -0500

    updated how it works

--- src/components/howitworks/HowItWorks.js
+++ src/components/howitworks/HowItWorks.js
@@ -13,13 +13,37 @@ const HowItWorks = () => {
                 to life! We will walk you through how our web development process works to ensure you know exactly what to expect.
                 </p>
                 <p>
-                  STEP 1:
+                  STEP 1: Fill Out Website Needs Evaluation Contact Form
                 </p>
                 <p>
-                  STEP 2:
+                  STEP 2: Choose Web Development Package
                 </p>
                 <p>
-                  STEP 3:
+                  STEP 3: Choose Web Hosting & Maintenance Package
+                </p>
+                <p>
+                  STEP 4: In Person or Zoom Meeting with Brian to Discuss Needs
+                </p>
+                <p>
+                  STEP 5: Sign Web Development, Hosting, and Maintenance Contract
+                </p>
+                <p>
+                  STEP 6: Pay 50% Deposit of Chosen Web Development Package
+                </p>
+                <p>
+                  STEP 7: Brian completes Web Development Stages with Constant Updates & Input
+                </p>
+                <p>
+                  STEP 8: Pay remaining 50% cost of Chosen Web Development Package
+                </p>
+                <p>
+                  STEP 9: Subscribe to Chosen Monthly Hosting and Maintenance Plan
+                </p>
+                <p>
+                  STEP 10: Your Website is live on the Internet & your files provided to you
+                </p>
+                <p>
+                  STEP 11: Brian updates your website as needed per your Chosen Maintenance Plan
                 </p>
                 <div>
                 
