df4eea91269c99f336490c3cead27fc20456711b
Brian Done <briguy2078@gmail.com>
2024-01-12 11:48:59 -0500

    how it works

--- src/components/howitworks/HowItWorks.js
+++ src/components/howitworks/HowItWorks.js
@@ -22,7 +22,9 @@ const HowItWorks = () => {
                   STEP 3: Choose Web Hosting & Maintenance Package
                 </p>
                 <p>
-                  STEP 4: In Person or Zoom Meeting with Brian to Discuss Needs
+                  STEP 4: In Person or Zoom Meeting with Brian to get to know each other. We will begin the
+                  Discovery Phase to discuss all of the elements of the website project scope to define success, 
+                  and work on goal identification. 
                 </p>
                 <p>
                   STEP 5: Sign Web Development, Hosting, and Maintenance Contract
@@ -31,7 +33,12 @@ const HowItWorks = () => {
                   STEP 6: Pay 50% Deposit of Chosen Web Development Package
                 </p>
                 <p>
-                  STEP 7: Brian completes Web Development Stages with Constant Updates & Input
+                  STEP 7: Brian completes Web Development Stages. This includes 3 Phases.<br/>
+                  A. Creative Phase: Brian creates a website concept, prototype, layout, and interface.<br/>
+                  B. Development Phase: Time to start coding and build the actual website! Brian will give you
+                  regular updates to make sure we stay in sync.
+                  C. Pre-Launch Phase: Extended testing of all website features to make sure everything is working
+                  perfectly and to make sure it's just right.
                 </p>
                 <p>
                   STEP 8: Pay remaining 50% cost of Chosen Web Development Package
@@ -40,10 +47,11 @@ const HowItWorks = () => {
                   STEP 9: Subscribe to Chosen Monthly Hosting and Maintenance Plan
                 </p>
                 <p>
-                  STEP 10: Your Website is live on the Internet & your files provided to you
+                  STEP 10: Your Website will now become live on the Internet & your website files will be provided to you
                 </p>
                 <p>
-                  STEP 11: Brian updates your website as needed per your Chosen Maintenance Plan
+                  STEP 11: Brian will update and maintain your website as needed per your Chosen Monthly Maintenance Plan. This
+                  includes making routine tweaks and updates as necessary. 
                 </p>
                 <div>
                 
