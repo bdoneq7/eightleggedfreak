031cbe97af5788bf6aeacf0c59a22e51c253cdd3
Brian Done <briguy2078@gmail.com>
2024-01-12 13:02:46 -0500

    icons

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -12,8 +12,10 @@ const Hero = () => {
                 <h2><FaFileCode className='iconmain'/><span> Custom</span> Web Development</h2>
                   
                     <p>Brian Done, the owner of 8LeggedFreak, will build you a quality custom react website that will help you achieve your website dreams!<br/>
+                    <br/>
                     Brian will give you the personalized attention required to translate your website vision into reality and provide the on-going support
                     to keep your website updated and keep you satisifed.
+                    <br/>
                     <br/>
                     Website Packages are conveniently offered to ensure a clear and straight-forward web development experience.
                     <br/>
--- src/components/howitworks/HowItWorks.js
+++ src/components/howitworks/HowItWorks.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './HowItWorksStyles.css';
 import { FcProcess } from "react-icons/fc";
-import { LuStepForward } from "react-icons/lu";
+import { GoPackage } from "react-icons/go";
+import { GrHostMaintenance } from "react-icons/gr";
+import { FaWpforms } from "react-icons/fa";
+import { MdMeetingRoom } from "react-icons/md";
+import { FaFileContract } from "react-icons/fa";
+import { MdAttachMoney } from "react-icons/md";
+import { CgWebsite } from "react-icons/cg";
+import { GrVmMaintenance } from "react-icons/gr";
 
 const HowItWorks = () => {
     return  (
@@ -13,47 +20,50 @@ const HowItWorks = () => {
                 8 Legged Freak is your one-stop shop for developing your new website and we are excited to be bring your vision
                 to life! We will walk you through how our web development process works to ensure you know exactly what to expect.
                 </p>
-                <p>
-                < LuStepForward className='iconmain' /> STEP 1: Fill Out Website Needs Evaluation Contact Form
-                </p>
-                <p>
-                  STEP 2: Choose Web Development Package
-                </p>
-                <p>
-                  STEP 3: Choose Web Hosting & Maintenance Package
-                </p>
-                <p>
-                  STEP 4: In Person or Zoom Meeting with Brian to get to know each other. We will begin the
+                <h3>
+                < GoPackage size='25' className='iconstep' /> STEP 1: Choose Web Development Package
+                </h3>
+                <h3>
+                < GrHostMaintenance size='25' className='iconstep' /> STEP 2: Choose Web Hosting & Maintenance Plan
+                </h3>
+                <h3>
+                < FaWpforms size='25' className='iconstep' /> STEP 3: Fill Out Website Needs Evaluation Contact Form
+                </h3>
+                <h3>
+                < MdMeetingRoom size='25' className='iconstep' /> STEP 4: In Person or Zoom Meeting with Brian to get to know each other.<br/>
+                 <br/>
+                 We will begin the
                   Discovery Phase to discuss all of the elements of the website project scope to define success, 
                   and work on goal identification. 
-                </p>
-                <p>
-                  STEP 5: Sign Web Development, Hosting, and Maintenance Contract
-                </p>
-                <p>
-                  STEP 6: Pay 50% Deposit of Chosen Web Development Package
-                </p>
-                <p>
-                  STEP 7: Brian completes Web Development Stages. This includes 3 Phases.<br/>
+                </h3>
+                <h3>
+                < FaFileContract size='25' className='iconstep' /> STEP 5: Sign Web Development, Hosting, and Maintenance Contract
+                </h3>
+                <h3>
+                < MdAttachMoney size='25' className='iconstep' /> STEP 6: Pay 50% Deposit of Chosen Web Development Package
+                </h3>
+                <h3>
+                < CgWebsite size='25' className='iconstep' /> STEP 7: Brian completes Web Development Stages. This includes 3 Phases.<br/>
+                <br/>
                   A. Creative Phase: Brian creates a website concept, prototype, layout, and interface.<br/>
                   B. Development Phase: Time to start coding and build the actual website! Brian will give you
-                  regular updates to make sure we stay in sync.
+                  regular updates to make sure we stay in sync.<br/>
                   C. Pre-Launch Phase: Extended testing of all website features to make sure everything is working
                   perfectly and to make sure it's just right.
-                </p>
-                <p>
-                  STEP 8: Pay remaining 50% cost of Chosen Web Development Package
-                </p>
-                <p>
-                  STEP 9: Subscribe to Chosen Monthly Hosting and Maintenance Plan
-                </p>
-                <p>
-                  STEP 10: Your Website will now become live on the Internet & your website files will be provided to you
-                </p>
-                <p>
-                  STEP 11: Brian will update and maintain your website as needed per your Chosen Monthly Maintenance Plan. This
+                </h3>
+                <h3>
+                < MdAttachMoney size='25' className='iconstep' /> STEP 8: Pay remaining 50% cost of Chosen Web Development Package
+                </h3>
+                <h3>
+                < MdAttachMoney size='25' className='iconstep' /> STEP 9: Subscribe to Chosen Monthly Hosting and Maintenance Plan
+                </h3>
+                <h3>
+                < CgWebsite size='25' className='iconstep' /> STEP 10: Your Website will now become live on the Internet & your website files will be provided to you
+                </h3>
+                <h3>
+                < GrVmMaintenance size='25' className='iconstep' /> STEP 11: Brian will update and maintain your website as needed per your Chosen Monthly Maintenance Plan. This
                   includes making routine tweaks and updates as necessary. 
-                </p>
+                </h3>
                 <div>
                 
                     
