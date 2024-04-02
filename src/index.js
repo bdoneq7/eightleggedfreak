b13418fc401a8ebb289bc8fdad639c07e23ecdce
Brian Done <briguy2078@gmail.com>
2024-04-01 20:36:09 -0400

    updated structure

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-import Protected from './components/Protected';
+
 import AboutBrianPage from './routes/AboutBrianPage';
 import ClientPortfolioPage from './routes/ClientPortfolioPage';
 import ServicesPackagesPage from './routes/ServicesPackagesPage';
@@ -15,8 +15,9 @@ import AccountPage from './routes/AccountPage';
 import { AuthContextProvider } from './context/AuthContext';
 
 ReactDOM.render(
-  <AuthContextProvider>
+  
   <BrowserRouter>
+  <AuthContextProvider>
     <Routes>
       <Route path='/' element={<App />} />
       <Route path='/aboutbrian' element={<AboutBrianPage />} />
@@ -24,10 +25,10 @@ ReactDOM.render(
       <Route path='/servicespackages' element={<ServicesPackagesPage />} />
       <Route path='/howitworks' element={<HowItWorksPage />} />
       <Route path='/contact' element={<ContactPage />} />
-      <Route path='/account' element={<Protected><AccountPage /></Protected>} />
+      <Route path='/account' element={<AccountPage />} />
     </Routes>
-  </BrowserRouter>
-  </AuthContextProvider>,
+    </AuthContextProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
